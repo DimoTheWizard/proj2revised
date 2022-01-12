acdba54c2f3b503d2b17b0d8a633c08218aec7c7
micriteo <teodorfolea@yahoo.com>
2022-01-12 21:36:58 +0100

    UserPanel :*

---
+++ userPanel.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="res/style.css">
+    <title>Falcon Hotel and Resturant</title>
+    <meta charset="utf-8">
+    <meta lang="en-us">
+</head>
+<body id = "admin">
+
+<?php
+include('res/elements/header.php');
+?>
+
+<?php
+include('res/elements/footer.php');
+?>
+
+<form method = "post">
+    <h1 class = "Tabletitle" > Activity </h1>
+      <table border="1" id="UserPanel">
+         <tr>
+             <th> ID    </th>
+             <th> email </th>
+             <th> username </th>
+             <th> userLevel </th>
+             <th> pathcert  </th>
+          </tr>
+      </table>
