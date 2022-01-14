d1a62d4c949b22971e2c4f83d62b2e38022a3729
DimoTheWizard <dimitri.vastenhout@gmail.com>
2022-01-14 12:00:57 +0100

    removed double session start in event calendar

--- event_calendar.php
+++ event_calendar.php
@@ -4,7 +4,6 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <?php session_start(); ?>
         <link rel="stylesheet" href="res/style.css">
         <title>Falcon Hotel and Resturant</title>
         <meta charset="utf-8">
